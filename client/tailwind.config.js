e81d9a8aee114d09f3f242ae347920d40d442ecb
Kanonball <kanonball@Kanonballs-Mac-mini.local>
2026-03-29 10:09:12 -0700

    Revert "Add xs:480px breakpoint; show search + subtitle inline at 480px+"
    This reverts commit 8e1cea3a3bcc28a4a2bd8c2f07db9e7e576ec8f1.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,14 +2,6 @@
 export default {
   content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
-    screens: {
-      xs: '480px',
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1280px',
-      '2xl': '1536px',
-    },
     extend: {
       fontFamily: {
         sans: ['Inter', 'system-ui', 'sans-serif'],
